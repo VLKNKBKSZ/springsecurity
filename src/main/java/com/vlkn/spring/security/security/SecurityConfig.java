39a0d5b5116b99a59afd1ee1dddc207cccc6510d
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2021-01-28 20:55:51 +0100

    created in memory user with a role, not working due password is not encoded yet

--- src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
+++ src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
@@ -1,13 +1,31 @@
 package com.vlkn.spring.security.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails james = User.builder()
+                .username("james")
+                .password("1")
+                .roles("STUDENT")//ROLE_STUDENT
+                .build();
+
+        return new InMemoryUserDetailsManager(james);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
