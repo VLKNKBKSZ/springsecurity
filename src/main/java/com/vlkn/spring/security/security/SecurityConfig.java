aaea2f4d7b6ce8cb4ce62ece54afab9e01461255
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2021-01-28 21:00:35 +0100

    created password encoder and encoded the inmemory password of the user

--- src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
+++ src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vlkn.spring.security.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public SecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     @Bean
@@ -20,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         UserDetails james = User.builder()
                 .username("james")
-                .password("1")
+                .password(passwordEncoder.encode("1"))
                 .roles("STUDENT")//ROLE_STUDENT
                 .build();
 
