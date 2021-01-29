942fa5f255934c748a26106bc49e58b87de4ddf4
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2021-01-29 21:50:59 +0100

    implemented permission based authorization

--- src/main/java/com/vlkn/spring/security/controllers/StudentManagementController.java
+++ src/main/java/com/vlkn/spring/security/controllers/StudentManagementController.java
@@ -21,21 +21,25 @@ public class StudentManagementController {
 
     @GetMapping
     public List<Student> getStudents() {
+        System.out.println("getStudents");
         return STUDENTS;
     }
 
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
+        System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
+        System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
     public void updateStudent(@PathVariable("studentId") Integer studentId,@RequestBody Student student) {
-        System.out.println(format("%s,%s",studentId, student));
+        System.out.println("updateStudent");
+        System.out.printf("%s,%s%n",studentId, student);
     }
 }
--- src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
+++ src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.vlkn.spring.security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.vlkn.spring.security.security.ApplicationUserPermission.STUDENT_WRITE;
 import static com.vlkn.spring.security.security.ApplicationUserRole.*;
 
 @Configuration
@@ -53,11 +55,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()// TODO: Will be learned in detail.
                 .authorizeRequests()
                 .antMatchers("/","/index","/css/*","/js/*")
                 .permitAll()
                 .antMatchers("/api/**")
                 .hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(STUDENT_WRITE.name())
+                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(STUDENT_WRITE.name())
+                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(STUDENT_WRITE.name())
+                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
