e49a14a01e1c6db3a8dc05071189ee590a9f35a9
VLKNKBKSZ <kabaksizvolkan@hotmail.com>
2021-02-01 20:05:31 +0100

    implemented remember-me cookie and extended the default value

--- src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
+++ src/main/java/com/vlkn/spring/security/security/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+import java.util.concurrent.TimeUnit;
+
 import static com.vlkn.spring.security.security.ApplicationUserRole.*;
 
 @Configuration
@@ -76,7 +78,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .permitAll()
-                .defaultSuccessUrl("/courses",true);
+                .defaultSuccessUrl("/courses",true)
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
+                .key("secureverysecuresupersecure");// defaults to 2 weeks.
                 // CANT LOGOUT with basic auth.
 //                .httpBasic();
 
